2c3e236e1a8e2d189cb26bbb6e587692fa9eec88
Herberth Andrade <andrade.engmecanica@gmail.com>
2022-09-16 22:46:08 -0300

    new modifies

--- App.jsx
+++ App.jsx
@@ -1,83 +1,10 @@
-import { useState } from 'react';
-import { StyleSheet, View, FlatList, Button } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-
-import GoalInput from './components/GoalInput';
-import GoalItem from './components/GoalItem';
+import { View, Text } from 'react-native'
+import React from 'react'
 
 export default function App() {
-  const [modalIsVisible, setModalIsVisible] = useState(false);
-  const [courseGoals, setCourseGoals] = useState([])
-
-  function startAddGoalHandler() {
-    setModalIsVisible(true)
-  }
-
-  function endAddGoalHandler() {
-    setModalIsVisible(false)
-  }
-
-  function addGoalHandler(enteredGoalText) {
-    setCourseGoals(currentCourseGoals => [
-      ...currentCourseGoals,
-      { text: enteredGoalText, id: Math.random().toString() }
-    ])
-    endAddGoalHandler()
-  }
-
-  function deleteGoalHandler(id) {
-    setCourseGoals(currentCourseGoals => {
-      return currentCourseGoals.filter(goal => goal.id !== id)
-    })
-  }
-
   return (
-    <>
-      <StatusBar style='light'/>
-      <View style={styles.appContainer}>
-        <View style={styles.buttonPrincipal}>
-          <Button
-          title='Add New Goal'
-          color="#a065ec"
-          onPress={startAddGoalHandler}/>
-        </View>
-        <GoalInput
-        visible={modalIsVisible}
-        onAddGoal={addGoalHandler}
-        onCancel={endAddGoalHandler}/>
-        <View  style={styles.goalsContainer}>
-          <FlatList
-            data={courseGoals}
-            renderItem={(itemData) => {
-              return <GoalItem
-              text={itemData.item.text}
-              id={itemData.item.id}
-              onDeleteItem={deleteGoalHandler} />
-            }}
-            keyExtractor={(item, index)=> {
-              return item.id;
-            }}
-            alwaysBounceVertical={false}
-          />
-        </View>
-      </View>
-    </>
+    <View>
+      <Text>App</Text>
+    </View>
   )
 }
-
-const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    padding: 80,
-    paddingHorizontal: 30,
-  },
-  goalsContainer: {
-    flex: 4
-  },
-  buttonPrincipal: {
-    width: '100%',
-    paddingHorizontal: 10,
-    marginBottom: 6,
-  }
-})
-
